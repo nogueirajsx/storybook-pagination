6fead5e11822a200d32c615c1c7e08d1122a6f02
Rodrigo Nogueira <nogueirajsx@gmail.com>
2020-07-01 23:43:47 -0300

    Paginação - storybook

---
+++ src/components/pagination/page.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import React from 'react'
+
+const Dots = () => <span>...</span>
+
+const Page = ({ page, pageLink, onClick }) => {
+    const Component = page === '...' ? Dots : 'a'
+    const href = page === '...' ? null : pageLink
+
+    const handleClick = !onClick ? null : (e) => {
+        e.preventDefault()
+        onClick(page)
+    }
+    
+    return (
+        <Component href={pageLink} onClick={handleClick}>
+            {page}
+        </Component>  
+    )     
+}
+
+export default Page
---
+++ src/components/repos/repos.story.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+import Repos from './index'
+
+storiesOf('Repos', module)
+  .add('with title prop', () => (
+    <Repos title='Favoritos' />
+  ))
+
+  .add('with repos', () => (
+    <div style={{ padding: '50px' }}>
+      <Repos
+        title='Favoritos'
+        repos={[{
+          link: 'https://github.com/nogueirajsx',
+          name: 'nogueirajsx'
+        }]}
+      />
+    </div>
+  ))
